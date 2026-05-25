8b868f62e4ee3c63b28f3a7d2ef3fb583751c212
Alan Inhuma <aspas_inhuma321@outlook.com>
2026-05-24 21:02:16 -0300

    Password lenght criado

--- src/assets/components/PasswordGenerator/index.jsx
+++ src/assets/components/PasswordGenerator/index.jsx
@@ -1,12 +1,19 @@
 import './style.css';
+import { useState } from 'react';
+
 
 const PasswordGenerator = () => {
+    const [passwordLenght, setPasswordLenght] = useState(8);
+
 
     return(
         <>
             <div className='slider'>
-                
+                <label htmlFor="Tamanho"></label>
 
+                <h3>{passwordLenght}</h3>
+                <input type="text" id='password-lenght' type='range' min={4} max={64} value={passwordLenght}
+                    onChange={({target}) => setPasswordLenght(target.value)}/>
 
             </div>
         </>
